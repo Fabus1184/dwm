9cb4897f7fe0f1c9f801e88c9b9708a709f0554a
Fabian Lippold <fabianlippold1184@gmail.com>
2021-10-28 13:25:02 +0200

    geht so

--- dwm.c
+++ dwm.c
@@ -777,14 +777,14 @@ drawbar(Monitor *m)
 		x += w;
 	}
 	
-	/*w = blw = TEXTW(m->ltsymbol);
+	w = blw = TEXTW(m->ltsymbol);
 	drw_setscheme(drw, scheme[SchemeNorm]);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
+			drw_text(drw, x, 0, w, bh, lrpad / 2, NULL, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
@@ -792,7 +792,7 @@ drawbar(Monitor *m)
 			drw_rect(drw, x, 0, w, bh, 1, 1);
 		}
 	}
-	*/
+	
 	drw_map(drw, m->barwin, 0, 0, m->ww, bh);
 }
 
